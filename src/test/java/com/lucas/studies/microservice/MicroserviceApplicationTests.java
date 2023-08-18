92f08d9b7d1908cab3eb1a48b83090633fc5b7c4
Lucas Farias <lucas.farias@darmlabs.ifce.edu.br>
2023-08-18 10:44:24 -0300

    first commit

---
+++ src/test/java/com/lucas/studies/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.studies.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
